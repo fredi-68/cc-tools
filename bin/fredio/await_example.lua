03fe841fca7b158645adb2ebdd5cd50a1a96226c
fredi_68 <jascha.hirsekorn@googlemail.com>
2021-11-04 15:23:33 +0100

    docs

--- bin/fredio/await_example.lua
+++ bin/fredio/await_example.lua
@@ -1,12 +1,30 @@
+--[[
+    FredIO await() Example.
+
+    This example demonstrates how to use libfredio.EventLoop().await()
+    to compose asynchronous function calls.
+]]
+
 loop = libfredio.EventLoop()
 
+--[[
+    Some asynchronous function that
+    computes a result.
+]]
 function my_function_1 ()
     os.sleep(1)
     return "Hello World"
 end
 
+--[[
+    Our main entrypoint.
+    This function calls my_function_2 using libfredio.EventLoop().await()
+    and prints the result. Creation and management of the underlying tasks
+    is abstracted away from the user.
+]]
 function my_function_2 ()
     print(loop.await(my_function_1))
 end
 
+-- run until our coroutine has finished
 loop.run_until_complete(coroutine.create(my_function_2))
--- bin/fredio/basic_example.lua
+++ bin/fredio/basic_example.lua
@@ -1,3 +1,11 @@
+--[[
+    Basic FredIO Concurrency Example
+
+    This example demonstrates how to achieve concurrent execution of asynchronous
+    functions using fredio. It features two functions simultaneously printing
+    numbers to the screen with varying amounts of delay.
+]]
+
 function my_function_1()
     for i = 1, 10 do
         print(i .. " from function 1")
@@ -14,8 +22,12 @@ function my_function_2()
     print("function 2 done")
 end
 
+-- first, create a loop instance
 local loop = libfredio.EventLoop()
+-- use loop.call() to automatically create coroutines from our
+-- functions and wrap them in tasks
 local t1 = loop.call(my_function_1)
 local t2 = loop.call(my_function_2)
 
+-- run the loop until all tasks have finished.
 loop.run_forever()
