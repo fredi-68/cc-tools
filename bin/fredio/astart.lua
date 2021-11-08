f15bf32f1e06d978f9692c8ea66de9d78f49cb00
marnixah <marnix@hagehoeve.com>
2021-11-08 09:15:55 +0100

    feat: find executable in path

--- bin/fredio/astart.lua
+++ bin/fredio/astart.lua
@@ -7,6 +7,17 @@
 
 loop = libfredio.EventLoop()
 
+-- Find executable in path
+local executable = nil
+local path_entries = string.gmatch(shell.path(), "[^:]+")
+for path_entry in path_entries do
+    local full_path = path_entry .. "/" .. arg[1]
+    if fs.exists(full_path) and not fs.isDir(full_path) then
+        executable = full_path
+        break
+    end
+end
+
 local _f = libfredio.async(function (path)
     dofile(path)
 end)
