7da502799a67a486c3c1935adb8f5b9fd43f62d5
fredi_68 <jascha.hirsekorn@googlemail.com>
2022-01-28 17:53:15 +0000

    added dhcp renew/release commands to ip

--- bin/frednet/ip.lua
+++ bin/frednet/ip.lua
@@ -8,6 +8,15 @@ if cmd == "address" then
     print("IPMC address: " .. libfrednet.get_local_host_ip())
 elseif cmd == "reload" then
     libfrednet.reload_config()
+elseif cmd == "dhcp" then
+    if arg[2] == "release" then
+        libfrednet.push_network_config({dhcp_lease = {}})
+        libfrednet.save_config()
+    elseif arg[2] == "renew" then
+        shell.run("systemctl start /etc/services/frednet/dhcp.lua")
+    else
+        print("Usage: frednet dhcp [release|renew]")
+    end
 else 
     print(textutils.serialize(libfrednet.get_network_config()))
 end
